4e987fb65f086d6edfc048491237df394e4eb30c
69IstominaEA <69IstominaEA@region.cbr.ru>
2026-08-21 02:43:56 +0300

    fix for checks

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaTopicsProperties.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaTopicsProperties.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum.collector.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "collector.kafka.topics")
 public class KafkaTopicsProperties {
     private String sensors;
