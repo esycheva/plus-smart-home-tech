8653e26c5e9bc9d2a91cb810df2e0b29340af674
69IstominaEA <69IstominaEA@region.cbr.ru>
2026-08-21 02:47:46 +0300

    fix for checks

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/CollectorApplication.java
@@ -2,10 +2,11 @@ package ru.yandex.practicum.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import ru.yandex.practicum.collector.config.KafkaTopicsProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableConfigurationProperties(KafkaTopicsProperties.class)
 public class CollectorApplication {
     public static void main(String[] args) {
