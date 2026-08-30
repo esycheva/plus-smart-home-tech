2140313af8c70b3880688a827294c503b01f8c8b
IstominaEA <istomina@example.com>
2026-08-31 02:41:56 +0300

    fix Deserializer

---
+++ telemetry/serialization/avro-schemas/src/main/java/ru/practicum/telemetry/HubEventDeserializer.java
@@ -0,0 +1,9 @@
+package ru.practicum.telemetry;
+
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+
+public class HubEventDeserializer extends BaseAvroDeserializer<HubEventAvro> {
+    public HubEventDeserializer() {
+        super(HubEventAvro.getClassSchema());
+    }
+}
