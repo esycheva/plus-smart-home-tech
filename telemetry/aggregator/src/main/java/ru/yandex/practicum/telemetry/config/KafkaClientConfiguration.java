1683655d112f60da7def5cb9736222f1e1fd7cdd
IstominaEA <istomina@example.com>
2026-09-03 17:05:35 +0300

    Вторая попытка вынести параметры в application.properties

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/config/KafkaClientConfiguration.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/config/KafkaClientConfiguration.java
@@ -1,32 +1,81 @@
 package ru.yandex.practicum.telemetry.config;
 
-import org.apache.avro.specific.SpecificRecordBase;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-
-import ru.practicum.telemetry.SensorEventDeserializer;
-import ru.practicum.telemetry.TelemetryAvroSerializer;
-
 import java.util.Properties;
 
-@Configuration
-@EnableConfigurationProperties(KafkaProperties.class)
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
 public class KafkaClientConfiguration {
-    @Bean
-    public Producer<String, SpecificRecordBase> kafkaProducer(KafkaProperties kafkaProperties) {
-        return new KafkaProducer<>(kafkaProperties.buildProducerProperties(null));
+
+    @Bean(destroyMethod = "close")
+    public Producer<String, SpecificRecordBase> kafkaProducer(
+            @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers,
+            @Value("${spring.kafka.producer.client-id:aggregator-producer}") String clientId,
+            @Value("${spring.kafka.producer.value-serializer}") String valueSerializer
+    ) {
+        Properties properties = new Properties();
+
+        properties.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers
+        );
+        properties.put(
+                ProducerConfig.CLIENT_ID_CONFIG,
+                clientId
+        );
+        properties.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
+        properties.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                valueSerializer
+        );
+
+        return new KafkaProducer<>(properties);
     }
 
-    @Bean
-    public Consumer<String, SpecificRecordBase> kafkaConsumer(KafkaProperties kafkaProperties) {
-        return new KafkaConsumer<>(kafkaProperties.buildConsumerProperties(null));
+    @Bean(destroyMethod = "close")
+    public Consumer<String, SpecificRecordBase> kafkaConsumer(
+            @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers,
+            @Value("${spring.kafka.consumer.client-id:aggregator-consumer}") String clientId,
+            @Value("${spring.kafka.consumer.group-id}") String groupId,
+            @Value("${spring.kafka.consumer.value-deserializer}") String valueDeserializer
+    ) {
+        Properties properties = new Properties();
+
+        properties.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers
+        );
+        properties.put(
+                ConsumerConfig.CLIENT_ID_CONFIG,
+                clientId
+        );
+        properties.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                groupId
+        );
+        properties.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class
+        );
+        properties.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                valueDeserializer
+        );
+
+        return new KafkaConsumer<>(properties);
     }
 }
