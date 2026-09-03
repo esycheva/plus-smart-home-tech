cddce3cb2dc925a1e35f74253b478ccf38cda511
IstominaEA <istomina@example.com>
2026-09-03 17:13:07 +0300

    for checks

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/config/KafkaClientConfiguration.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/config/KafkaClientConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
 public class KafkaClientConfiguration {
 
     @Bean(destroyMethod = "close")
